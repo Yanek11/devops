42d2f7e701a7ffe828309f9b5f711d7f8ff5af09
Yanek11 <110762418+Yanek11@users.noreply.github.com>
2023-01-09 19:42:43 +0100

    1 changevvv

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 #  ERROR HANDLING: TRY ... EXCEPT
 
-calculation_to_units=24
+calculation_to_units = 24
 name_of_unit = "hours"  # this variable is GLOBAL
 
 
 def days_to_units(num_of_days):
     return f"{num_of_days} days are {num_of_days*calculation_to_units} {name_of_unit}"
+
+
 """ BEST PRACTICE - block below can be put as a function.
 if user_input.isdigit():
     user_input_number=int(user_input)
@@ -20,13 +22,13 @@ def validate_and_execute():  # function is used to check if the input is a digit
     try:  # try/except is used to check  several lines for errors. if you cannot use IF...ELSE statement
         user_input_number = int(user_input)
         if user_input_number > 0:
-            calculated_value=days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
             print("you entered a 0, enter a positive number")
     except ValueError:
-
         print("your input is not a valid number")
+
+
 user_input = input("hey input some data and i will convert it to hours\n")
 validate_and_execute()
-
