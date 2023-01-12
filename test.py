5599b506b2681314f3fa00039c13b34ce6849218
Yanek11 <110762418+Yanek11@users.noreply.github.com>
2023-01-12 19:28:20 +0100

    sii laptop 12/01/2023

--- test.py
+++ test.py
@@ -47,7 +47,7 @@ while user_input != "exit":
     print(type(user_input.split(",")))  # shows type of the user input converted
     print(user_input.split(","))
     for num_of_days_element in user_input.split(","):  # user input with commas will be converted to list data type
-    # for num_of_days_element in user_input.split():  # user input (String) with spaces
+            # for num_of_days_element in user_input.split():  # user input (String) with spaces
 
     ''' #4 function is called and evaluated '''
     validate_and_execute()
