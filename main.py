b69ba95694272735d6549524edcff94b82926d1c
Yanek11 <110762418+Yanek11@users.noreply.github.com>
2023-01-28 21:29:55 +0100

    sii laptop 28/01/2023

--- main.py
+++ main.py
@@ -388,3 +388,21 @@ my_list.append("April111")
 print(my_list[3])
 print(my_list)
 
+# SET  28 January 2023
+my_set = {"January", "March", "February", "January"}
+
+my_set.add("April") # adding
+# for element in my_set:
+print(my_set)
+
+my_set.remove("January")  # removing both occurrences of January
+print(my_set)
+
+""" removing from LISTS """
+
+my_list=["January", "March", "February", "March"]
+print(my_list)
+my_list.remove("March")   # removing only FIRST occurrence of March
+print(my_list)
+
+
