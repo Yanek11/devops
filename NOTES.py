17070a278a10452e4bd3e988649b67922a1fd4e7
Yanek11 <110762418+Yanek11@users.noreply.github.com>
2023-02-12 18:43:09 +0100

    sii laptop 12/02/2023

---
+++ NOTES.py
@@ -0,0 +1,15 @@
+"""
+12/02/2023
+
+SETS
+ - do not allow duplicates
+ - output is not ordered
+
+LISTS
+- allow duplicates
+- if a duplicated value is removed, only first occurrence is removed
+"""
+
+"""Built-In Functions
+
+"""
